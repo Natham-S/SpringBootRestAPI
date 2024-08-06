0bc1d9a1243d1099e769f93664d1b1fbf9b9d442
Sainath S <sainath.shanigaram444@gmail.com>
2024-08-06 20:04:12 +0530

    Initial commit

---
+++ src/main/java/com/example/restapi/Student.java
@@ -0,0 +1,46 @@
+package com.example.restapi;
+
+import org.springframework.stereotype.Controller;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int rollNo;
+	private String name;
+	private float percentage;
+	private String branch;
+	
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(float percentage) {
+		this.percentage = percentage;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	
+	
+}
